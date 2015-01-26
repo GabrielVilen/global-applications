fc33d5c686998b074d4bbba8b74a32aa52b709f1
Kim <Kim@130.229.160.193>
2015-01-26 14:57:47 +0100

    ändrade strukturen till layer & införde nya klasser som server å databas impl

---
+++ IV1201Projekt/src/main/java/se/kth/iv1201projekt/businesslogic/ASServer.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package se.kth.iv1201projekt.businesslogic;
+
+/**
+ * This class will act as a controller between the presentation layer (web 
+ * pages) and the integration layer.
+ * 
+ * @author Kim
+ */
+public class ASServer {
+    
+}
