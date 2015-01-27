bf80d2a73bc74bb7d33475b85864a8350bf18f61
Kim <Kim@130.229.160.193>
2015-01-27 11:15:16 +0100

    controller skelett 2

---
+++ IV1201Projekt/src/main/java/se/kth/iv1201projekt/integration/ASController.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package se.kth.iv1201projekt.integration;
+
+import se.kth.iv1201projekt.businesslogic.UserBean;
+import se.kth.iv1201projekt.util.Job;
+import se.kth.iv1201projekt.util.LoginErrorException;
+
+/**
+ * Should translate DTO:s into appropriate beans and also handle flow control.
+ * This class should handle the ACID principles for the database.
+ * 
+ * @author Kim
+ */
+public class ASController {
+    
+    public UserBean login(String username, String password) throws LoginErrorException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+}
