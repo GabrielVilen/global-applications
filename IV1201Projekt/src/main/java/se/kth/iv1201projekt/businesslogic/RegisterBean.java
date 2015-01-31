64ab3e1813ecb5f76e75deb933c81ddc8456c4c4
Samy <Syrish_92@hotmail.com>
2015-01-31 15:49:57 +0100

    Skapade en bean för att acceptera input ifrån register och satte dit Required fält i register.xhtml

--- IV1201Projekt/src/main/java/se/kth/iv1201projekt/businesslogic/RegisterBean.java
+++ IV1201Projekt/src/main/java/se/kth/iv1201projekt/businesslogic/RegisterBean.java
@@ -6,15 +6,113 @@
 package se.kth.iv1201projekt.businesslogic;
 
 import java.io.Serializable;
+import java.util.Date;
+import java.util.List;
 import javax.enterprise.context.SessionScoped;
 import javax.inject.Named;
 
 /**
- *
- * @author Gabriel
+ * A bean for the registration of new applicants.
+ * 
+ * @author Samy
  */
 
 @Named("registerBean")
+@SessionScoped
 public class RegisterBean implements Serializable {
+    
+    private String firstname;
+    private String lastname;
+    private String ssn;
+    private String email;
+    private String role;
+    private String birth;
+    private int yearsofexperience;
+    private String fromtime;
+    private String totime;
+    private List competences;
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getSsn() {
+        return ssn;
+    }
+
+    public void setSsn(String ssn) {
+        this.ssn = ssn;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public String getBirth() {
+        return birth;
+    }
+
+    public void setBirth(String birth) {
+        this.birth = birth;
+    }
+
+
+    public int getYearsofexperience() {
+        return yearsofexperience;
+    }
+
+    public void setYearsofexperience(int yearsofexperience) {
+        this.yearsofexperience = yearsofexperience;
+    }
+
+    public String getFromtime() {
+        return fromtime;
+    }
+
+    public void setFromtime(String fromtime) {
+        this.fromtime = fromtime;
+    }
+
+    public String getTotime() {
+        return totime;
+    }
+
+    public void setTotime(String totime) {
+        this.totime = totime;
+    }
+    
+    public List getCompetences() {
+        return competences;
+    }
+
+    public void setCompetences(List competences) {
+        this.competences = competences;
+    }
+    
+    
     
 }
