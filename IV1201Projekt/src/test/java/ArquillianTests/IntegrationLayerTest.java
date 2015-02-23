58928b3df95d8debe1875367435461b41aff5434
Kim Borak <borak@kth.se>
2015-02-23 14:16:32 +0100

    Added arquillian setup. Progress in setup but not working yet. Current error is probably in the container setup. "Could not invoke deployment method" is thrown when arquillian.xml container is not default and when it is default, then the error is "could not create new instance of class eventtestrunneradaptor".

---
+++ IV1201Projekt/src/test/java/ArquillianTests/IntegrationLayerTest.java
@@ -0,0 +1,85 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ArquillianTests;
+
+import java.io.File;
+import javax.ejb.EJB;
+import junit.framework.Assert;
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.shrinkwrap.api.Archive;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.asset.EmptyAsset;
+import org.jboss.shrinkwrap.api.spec.WebArchive;
+import se.kth.iv1201projekt.integration.ASDBController;
+import se.kth.iv1201projekt.integration.ASJPADatabaseImpl;
+import se.kth.iv1201projekt.integration.model.Person;
+import se.kth.iv1201projekt.integration.model.User;
+import se.kth.iv1201projekt.util.LoginErrorException;
+
+/**
+ *
+ * @author Kim
+ */
+@RunWith(Arquillian.class)
+public class IntegrationLayerTest {
+
+    public static final String WEBAPP_SRC = "src/main/webapp";
+    public static final String USERNAME_TEST = "borg";
+    public static final String PASSWORD_TEST = "pass";
+    @EJB
+    private ASJPADatabaseImpl db;
+    
+    /*
+     @Deployment
+     public static WebArchive createDeployment() {
+     return ShrinkWrap.create(WebArchive.class)
+     .addClass(ASJPADatabaseImpl.class)
+     //.addAsManifestResource(EmptyAsset.INSTANCE, "beans.xml")
+     .addAsWebInfResource(new File(WEBAPP_SRC, "WEB-INF/faces-config.xml"))
+     .addAsWebInfResource(new File(WEBAPP_SRC, "WEB-INF/web.xml"));
+     }
+     */
+
+    @Deployment
+    public static Archive<?> createDeployment() {
+        WebArchive war = ShrinkWrap.create(WebArchive.class)
+                .addClass(ASJPADatabaseImpl.class)
+                .addClass(ASDBController.class)
+                .addClass(Person.class)
+                .addClass(User.class)
+                .addAsResource("test-persistence.xml", "META-INF/persistence.xml")
+                .addAsWebInfResource(EmptyAsset.INSTANCE, "beans.xml")
+                .addAsWebInfResource(new File(WEBAPP_SRC, "WEB-INF/faces-config.xml"))
+                .addAsWebInfResource(new File(WEBAPP_SRC, "WEB-INF/web.xml"));
+
+        war.writeTo(System.err, org.jboss.shrinkwrap.api.formatter.Formatters.VERBOSE);
+        System.err.println();
+        return war;
+    }
+
+    @Test
+    public void loginTest() {
+        /*
+         try {
+         Person person = db.login(USERNAME_TEST, PASSWORD_TEST);
+         if(person == null) 
+         Assert.fail("A login attempt failed. Error: Person is null.");
+         } catch (LoginErrorException ex) {
+         Assert.fail("A login attempt failed. Error: " + ex.getMessage());        
+         }
+        
+         try {
+         Person person = db.login("noneExistingUser", "pass");
+         Assert.fail("A failed attempt to login should throw an "
+         + "LoginErrorException. Error: ");
+         } catch (LoginErrorException ex) {
+         //Success 
+         }*/
+    }
+}
