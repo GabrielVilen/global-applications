280029d52f3346874f740c4b70943405b0dfc48e
Kim Borak <borak@kth.se>
2015-02-23 16:55:46 +0100

    Arquillian setup again. Builds and succeeds with zero declared test method but deployment method works. Seems to be a connection error with JPA.  (1) ArquillianServletRunner not found. (2)Could not resolve a persistence unit corresponding to the persistence-context-ref-name [se.kth.iv1201projekt.integration.ASJPADatabaseImpl/entityManager]

--- IV1201Projekt/src/test/java/ArquillianTests/IntegrationLayerTest.java
+++ IV1201Projekt/src/test/java/ArquillianTests/IntegrationLayerTest.java
@@ -16,6 +16,8 @@ import org.junit.runner.RunWith;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
 import org.jboss.shrinkwrap.api.asset.EmptyAsset;
 import org.jboss.shrinkwrap.api.spec.WebArchive;
+import org.junit.Ignore;
+import se.kth.iv1201projekt.businesslogic.UserBean;
 import se.kth.iv1201projekt.integration.ASDBController;
 import se.kth.iv1201projekt.integration.ASJPADatabaseImpl;
 import se.kth.iv1201projekt.integration.model.Person;
@@ -29,23 +31,6 @@ import se.kth.iv1201projekt.util.LoginErrorException;
 @RunWith(Arquillian.class)
 public class IntegrationLayerTest {
 
-    public static final String WEBAPP_SRC = "src/main/webapp";
-    public static final String USERNAME_TEST = "borg";
-    public static final String PASSWORD_TEST = "pass";
-    @EJB
-    private ASJPADatabaseImpl db;
-    
-    /*
-     @Deployment
-     public static WebArchive createDeployment() {
-     return ShrinkWrap.create(WebArchive.class)
-     .addClass(ASJPADatabaseImpl.class)
-     //.addAsManifestResource(EmptyAsset.INSTANCE, "beans.xml")
-     .addAsWebInfResource(new File(WEBAPP_SRC, "WEB-INF/faces-config.xml"))
-     .addAsWebInfResource(new File(WEBAPP_SRC, "WEB-INF/web.xml"));
-     }
-     */
-
     @Deployment
     public static Archive<?> createDeployment() {
         WebArchive war = ShrinkWrap.create(WebArchive.class)
@@ -53,33 +38,38 @@ public class IntegrationLayerTest {
                 .addClass(ASDBController.class)
                 .addClass(Person.class)
                 .addClass(User.class)
+                .addClass(LoginErrorException.class)
                 .addAsResource("test-persistence.xml", "META-INF/persistence.xml")
-                .addAsWebInfResource(EmptyAsset.INSTANCE, "beans.xml")
-                .addAsWebInfResource(new File(WEBAPP_SRC, "WEB-INF/faces-config.xml"))
-                .addAsWebInfResource(new File(WEBAPP_SRC, "WEB-INF/web.xml"));
+                .addAsWebInfResource(EmptyAsset.INSTANCE, "beans.xml");
 
         war.writeTo(System.err, org.jboss.shrinkwrap.api.formatter.Formatters.VERBOSE);
         System.err.println();
         return war;
     }
 
+    public static final String WEBAPP_SRC = "src/main/webapp";
+    public static final String USERNAME_TEST = "borg";
+    public static final String PASSWORD_TEST = "pass";
+    //@EJB private ASJPADatabaseImpl db;
+    
+    //@Ignore
     @Test
-    public void loginTest() {
-        /*
-         try {
-         Person person = db.login(USERNAME_TEST, PASSWORD_TEST);
-         if(person == null) 
-         Assert.fail("A login attempt failed. Error: Person is null.");
-         } catch (LoginErrorException ex) {
-         Assert.fail("A login attempt failed. Error: " + ex.getMessage());        
-         }
-        
-         try {
-         Person person = db.login("noneExistingUser", "pass");
-         Assert.fail("A failed attempt to login should throw an "
-         + "LoginErrorException. Error: ");
-         } catch (LoginErrorException ex) {
-         //Success 
-         }*/
+    public void loginTest() {/*
+        try {
+            Person person = db.login(USERNAME_TEST, PASSWORD_TEST);
+            if (person == null) {
+                Assert.fail("A login attempt failed. Error: Person is null.");
+            }
+        } catch (LoginErrorException ex) {
+            Assert.fail("A login attempt failed. Error: " + ex.getMessage());
+        }
+
+        try {
+            Person person = db.login("noneExistingUser", "pass");
+            Assert.fail("A failed attempt to login should throw an "
+                    + "LoginErrorException. Error: ");
+        } catch (LoginErrorException ex) {
+            //Success 
+        }*/
     }
 }
