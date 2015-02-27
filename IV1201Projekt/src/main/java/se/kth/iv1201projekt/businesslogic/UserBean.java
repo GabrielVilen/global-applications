7e18a7b22c1bc05eced58aa4839032f94efe0653
Kim Borak <borak@kth.se>
2015-02-27 13:40:45 +0100

    Added javadocs.

--- IV1201Projekt/src/main/java/se/kth/iv1201projekt/businesslogic/UserBean.java
+++ IV1201Projekt/src/main/java/se/kth/iv1201projekt/businesslogic/UserBean.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package se.kth.iv1201projekt.businesslogic;
 
 import java.io.Serializable;
@@ -15,7 +10,8 @@ import se.kth.iv1201projekt.integration.model.Person;
 import se.kth.iv1201projekt.util.LoggerUtil;
 
 /**
- *
+ * This bean is used for logging in service and to store the user's personal
+ * information.
  * @author Gabriel
  */
 @Named("userBean")
@@ -24,11 +20,15 @@ public class UserBean implements Serializable {
 
     @EJB
     private ASDBController controller;
-
     private String username;
     private String password;
     private Person person;
 
+    /**
+     * Logs in the user and fetches the user's information.
+     * Username and password need to be set before calling this method.
+     * @return A status text which will be handled by the JSF.
+     */
     public String login() {
         System.out.println("controller=" + controller + "username=" + username + "password=" + password);
         if (username == null || password == null) {
@@ -45,15 +45,30 @@ public class UserBean implements Serializable {
         }
     }
     
+    /**
+     * Applies for the specified job with the current logged in user.
+     * TODO: Reference the person who applies.
+     * @param id The job's id.
+     */
     public void applyForJob(int id) {
         controller.applyForJob(id);
     }
   
+    /**
+     * Logouts the user by resetting the beans state.
+     * @return The page to redirect to.
+     */
     public String logout() {
         person = null;
+        username = null;
+        password = null;
         return "index.xhtml";
     }
 
+    /**
+     * Checks the beans status to determine if the user is logged in.
+     * @return True if logged in else False.
+     */
     public boolean isLoggedIn() {
         return person != null;
     }
@@ -62,6 +77,10 @@ public class UserBean implements Serializable {
         return username;
     }
 
+    /**
+     * Store the username for further use
+     * @param username The username to use for operations.
+     */
     public void setUsername(String username) {
         this.username = username;
     }
@@ -70,6 +89,11 @@ public class UserBean implements Serializable {
         return password;
     }
 
+    /**
+     * Store the password as an encrypted password for further use.
+     * @param password The password to use for operations. This method will 
+     * encrypt the password.
+     */
     public void setPassword(String password) {
         StrongPasswordEncryptor passwordEncryptor = new StrongPasswordEncryptor();
         String encryptedPassword = passwordEncryptor.encryptPassword(password);
