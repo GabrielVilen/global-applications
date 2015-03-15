492d35a3ba46f595c0a41462a35a6e0db3d1f14d
Gabriel <gabriel.vilen@gmail.com>
2015-03-15 19:25:45 +0100

    LanguageBeanTest again

--- IV1201Projekt/src/test/java/MockitoTests/BusinesslogicTests/LanguageBeanTest.java
+++ IV1201Projekt/src/test/java/MockitoTests/BusinesslogicTests/LanguageBeanTest.java
@@ -2,6 +2,7 @@ package MockitoTests.BusinesslogicTests;
 
 import MockitoTests.UtilTests.ContextMocker;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import java.util.Locale;
 import java.util.Map;
@@ -22,52 +23,52 @@ import se.kth.iv1201projekt.integration.model.Job;
 import se.kth.iv1201projekt.util.LoggerUtil;
 
 /**
- * Tests of the JobBean class
+ * Tests of the LanguageBean class.
  *
  * @author Gabriel
  */
-//@Ignore
 @RunWith(MockitoJUnitRunner.class)
 public class LanguageBeanTest {
-
-//    public static class LangaugeStaticTest {
-//
-//        public static void staticInit() {
-//            languageBean.staticInit();
-//        }
-//    }
 
     private static LanguageBean languageBean;
     private final static String EN = "en";
     private final static String SV = "sv";
     private int itemsInList;
 
-    private List<Job> jobList;
+    @Mock
+    private Job job1, job2;
     @Mock
     private ASDBController controller;
 
-//    @BeforeClass
-//    public static void setUpBeforeClass() {
-//        Mock.redefineMethods(LanguageBean.class, LangaugeStaticTest.class);
-//        
-//    }
-
+    /**
+     * Set up the language and job list.
+     */
     @Before
     public void setUp() {
-        System.out.println("seTUp running");
         try {
+            itemsInList = 2;
+
             ArrayList<Locale> locList = new ArrayList();
-            locList.add(new Locale(SV,""));
+            locList.add(new Locale(SV, ""));
             FacesContext context = ContextMocker.mockFacesContext();
             when(context.getApplication().getDefaultLocale()).thenReturn(Locale.ENGLISH);
             when(context.getApplication().getSupportedLocales()).thenReturn(locList.iterator());
             languageBean = new LanguageBean();
-            itemsInList = 2;
+
+            List<Job> jobList = new ArrayList<>();
+            job1 = new Job(1, "type_1", "information_1", new Date(), new Date(), "name_1", 1);
+            job2 = new Job(2, "type_2", "information_2", new Date(), new Date(), "name_2", 1);
+            jobList.add(job1);
+            jobList.add(job2);
+            Mockito.when(controller.getAllJobs(languageBean.getLocale())).thenReturn(jobList);
         } catch (Exception e) {
             LoggerUtil.logTest(e, this);
         }
     }
 
+    /**
+     * Test changing the locale (language) in the language bean.
+     */
     @Test
     public void testChangeLocale() {
         UIComponent component = Mockito.mock(UIComponent.class);
@@ -78,27 +79,39 @@ public class LanguageBeanTest {
 
     }
 
+    /**
+     * Test if the job list contains the correct data.
+     */
     @Test
     public void testJobList() {
-        jobList = controller.getAllJobs(languageBean.getLocale());
+        List<Job> jobList = controller.getAllJobs(languageBean.getLocale());
         Assert.assertNotNull(jobList);
-        for (Job jobList1 : jobList) {
-            System.out.println(jobList1);
-        }
-        if (!(jobList.contains(EN) || jobList.contains(SV))) {
-            Assert.fail("Joblist does not contain 'en' or 'sv'");
+
+        if (!(jobList.contains(job1) || jobList.contains(job2))) {
+            Assert.fail("Joblist does not contain  " + job1 + "  or " + job2);
         }
         if (jobList.size() != itemsInList) {
             Assert.fail("Size of jobList = " + jobList.size() + ", should be " + itemsInList);
         }
+        for (Job job : jobList) {
+            Assert.assertNotNull(job.getName());
+            Assert.assertNotNull(job.getInformation());
+            Assert.assertNotNull(job.getType());
+            Assert.assertNotNull(job.getId());
+            Assert.assertNotNull(job.getVersion());
+            Assert.assertNotNull(job.getStartDate());
+        }
     }
 
+    /**
+     * Test if the language map contains the correct data.
+     */
     @Test
     public void testLanguages() {
         Map langs = languageBean.getLanguages();
         Assert.assertNotNull(langs);
 
-        if (!(langs.containsKey(new Locale(SV,"").getDisplayLanguage()) || langs.containsKey((Locale.ENGLISH).getDisplayLanguage()))) {
+        if (!(langs.containsKey(new Locale(SV, "").getDisplayLanguage()) || langs.containsKey((Locale.ENGLISH).getDisplayLanguage()))) {
             Assert.fail("language map does not contain 'sv' or 'en");
         }
         if (langs.size() != itemsInList) {
